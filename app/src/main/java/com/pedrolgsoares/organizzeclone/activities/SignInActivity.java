811d87061d4bb51625cfd9f463682991aaafaf18
pedrolgomess <pedro.lucas@guaraves.com.br>
2023-07-27 10:07:22 -0300

    Realizando sign in

--- app/src/main/java/com/pedrolgsoares/organizzeclone/activities/SignInActivity.java
+++ app/src/main/java/com/pedrolgsoares/organizzeclone/activities/SignInActivity.java
@@ -3,14 +3,30 @@ package com.pedrolgsoares.organizzeclone.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.pedrolgsoares.organizzeclone.R;
 
 public class SignInActivity extends AppCompatActivity {
-
+    private EditText name,password;
+    private Button buttonSignin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        name = findViewById(R.id.nameText);
+        password = findViewById(R.id.passwordText);
+        buttonSignin = findViewById(R.id.buttonSignin);
+
+        buttonSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(SignInActivity.this,"FAZENDO LOGIN",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
