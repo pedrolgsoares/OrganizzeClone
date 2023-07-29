7bf46173d0a126c9024bb46ef6e3c52e050827c3
pedrolgsoares <pedrodevmob@gmail.com>
2023-07-28 22:11:06 -0300

    Aplicando botões de action

--- app/src/main/java/com/pedrolgsoares/organizzeclone/activities/PrincipalActivity.java
+++ app/src/main/java/com/pedrolgsoares/organizzeclone/activities/PrincipalActivity.java
@@ -4,25 +4,55 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.pedrolgsoares.organizzeclone.R;
 
 public class PrincipalActivity extends AppCompatActivity {
-    private FloatingActionButton fab;
+    private FloatingActionButton morefab, incomefab,outgoingfab;
+    private TextView despesastext,receitastext;
+    private Boolean isAllFabsVisible;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
 
-        fab = findViewById(R.id.floatingActionButton);
+        morefab = findViewById(R.id.morefab);
+        incomefab = findViewById(R.id.incomefab);
+        outgoingfab = findViewById(R.id.outgoingfab);
 
-        fab.setOnClickListener(new View.OnClickListener() {
+        despesastext = findViewById(R.id.despesastext);
+        receitastext = findViewById(R.id.receitastext);
+
+
+        incomefab.setVisibility(View.GONE);
+        outgoingfab.setVisibility(View.GONE);
+
+        despesastext.setVisibility(View.GONE);
+        receitastext.setVisibility(View.GONE);
+
+        isAllFabsVisible = false;
+
+
+        morefab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Snackbar.make(v, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                if (!isAllFabsVisible) {
+                    incomefab.show();
+                    outgoingfab.show();
+                    despesastext.setVisibility(View.VISIBLE);
+                    receitastext.setVisibility(View.VISIBLE);
+                    isAllFabsVisible = true;
+                } else {
+                    incomefab.hide();
+                    outgoingfab.hide();
+                    despesastext.setVisibility(View.GONE);
+                    receitastext.setVisibility(View.GONE);
+                    isAllFabsVisible = false;
+                }
             }
         });
     }
