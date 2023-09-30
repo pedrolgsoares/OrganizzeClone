43077984ba72a5ed353372ed5783d97b9b8780ca
pedrolgsoares <pedrodevmob@gmail.com>
2023-09-30 11:15:13 -0300

    Alterando nomes de id

--- app/src/main/java/com/pedrolgsoares/organizzeclone/activities/OutGoingActivity.java
+++ app/src/main/java/com/pedrolgsoares/organizzeclone/activities/OutGoingActivity.java
@@ -11,21 +11,22 @@ import com.pedrolgsoares.organizzeclone.R;
 import com.pedrolgsoares.organizzeclone.helper.DataCustom;
 
 public class OutGoingActivity extends AppCompatActivity {
-    private EditText ETTotalValue;
-    private TextInputEditText TiedtData,TiedtCategoria,TiedtDescricao;
+    private EditText eTTotalValue;
+    private TextInputEditText tiedtData,tiedtCategoria,tiedtDescricao;
     private FloatingActionButton fabSalvar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_out_going);
 
-        ETTotalValue = findViewById(R.id.ETTotalValue);
-        TiedtData = findViewById(R.id.TiedtData);
-        TiedtCategoria = findViewById(R.id.TiedtCategoria);
-        TiedtDescricao = findViewById(R.id.TiedtDescricao);
+        eTTotalValue   = findViewById(R.id.eTTotalValue);
+        tiedtData      = findViewById(R.id.tiedtData);
+        tiedtCategoria = findViewById(R.id.tiedtCategoria);
+        tiedtDescricao = findViewById(R.id.tiedtDescricao);
+        fabSalvar      = findViewById(R.id.fabSalvar);
 
         // configurando a data atual
-        TiedtData.setText(DataCustom.getDataAtual());
+        tiedtData.setText(DataCustom.getDataAtual());
 
     }
 }
