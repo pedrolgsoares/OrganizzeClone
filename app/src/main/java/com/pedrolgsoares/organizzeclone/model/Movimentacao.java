92c91e8cc3fab1eb57514723d0debbb6ed351d2a
pedrolgsoares <pedrodevmob@gmail.com>
2023-10-24 21:18:16 -0300

    Objeto responsável pela movimentação das despesas ou receitas

---
+++ app/src/main/java/com/pedrolgsoares/organizzeclone/model/Movimentacao.java
@@ -0,0 +1,46 @@
+package com.pedrolgsoares.organizzeclone.model;
+
+public class Movimentacao {
+    private String categoria;
+    private String data;
+    private String descricao;
+    private Double valor;
+
+    public Movimentacao() {
+    }
+
+    public void salvar(){
+
+    }
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+}
