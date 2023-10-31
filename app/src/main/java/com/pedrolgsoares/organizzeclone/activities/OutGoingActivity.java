bb460541a7e0d1dac51c0a60ecb34c53af686b2e
pedrolgsoares <pedrodevmob@gmail.com>
2023-10-30 21:30:26 -0300

    Resta por no mês correto

--- app/src/main/java/com/pedrolgsoares/organizzeclone/activities/OutGoingActivity.java
+++ app/src/main/java/com/pedrolgsoares/organizzeclone/activities/OutGoingActivity.java
@@ -3,17 +3,21 @@ package com.pedrolgsoares.organizzeclone.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.pedrolgsoares.organizzeclone.R;
 import com.pedrolgsoares.organizzeclone.helper.DataCustom;
+import com.pedrolgsoares.organizzeclone.model.Movimentacao;
+import com.pedrolgsoares.organizzeclone.model.Usuario;
 
 public class OutGoingActivity extends AppCompatActivity {
     private EditText eTTotalValue;
     private TextInputEditText tiedtData,tiedtCategoria,tiedtDescricao;
     private FloatingActionButton fabSalvar;
+    private Movimentacao movimentacao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,5 +32,22 @@ public class OutGoingActivity extends AppCompatActivity {
         // configurando a data atual
         tiedtData.setText(DataCustom.getDataAtual());
 
+        // clique para incluir despesas
+        fabSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                movimentacao = new Movimentacao();
+                movimentacao.setValor(Double.parseDouble(eTTotalValue.getText().toString()));
+
+                String data = tiedtData.getText().toString();
+                movimentacao.setDescricao(data);
+
+                movimentacao.setCategoria(tiedtCategoria.getText().toString());
+                movimentacao.setDescricao(tiedtDescricao.getText().toString());
+                movimentacao.setTipo("d");
+
+                movimentacao.salvar();
+            }
+        });
     }
 }
--- app/src/main/java/com/pedrolgsoares/organizzeclone/model/Movimentacao.java
+++ app/src/main/java/com/pedrolgsoares/organizzeclone/model/Movimentacao.java
@@ -1,16 +1,31 @@
 package com.pedrolgsoares.organizzeclone.model;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.pedrolgsoares.organizzeclone.config.ConfiguracaoFirebase;
+import com.pedrolgsoares.organizzeclone.helper.Base64Custom;
+
 public class Movimentacao {
     private String categoria;
     private String data;
     private String descricao;
     private Double valor;
+    private String tipo;
+
+
 
     public Movimentacao() {
     }
 
     public void salvar(){
-
+        FirebaseAuth autenticacao = ConfiguracaoFirebase.getAutenticacao(); // realiza a comunicação
+        String idUsuario = Base64Custom.codificarBase64( autenticacao.getCurrentUser().getEmail() );
+        DatabaseReference firebase = ConfiguracaoFirebase.getFirebaseDatabase();
+        firebase.child("movimentacao")
+                .child(idUsuario)
+                .child("032023")
+                .push()
+                .setValue(this);
     }
     public String getCategoria() {
         return categoria;
@@ -42,5 +57,13 @@ public class Movimentacao {
 
     public void setValor(Double valor) {
         this.valor = valor;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
     }
 }
